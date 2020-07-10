7c83eeb600ae143c42c3322cee6961af781c60cf
kfwojton <kfwojton@gmail.com>
2020-07-10 17:23:42 -0400

    longer dates

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,136 +1,127 @@
-import React, { PureComponent } from 'react';
-import { Table } from 'reactstrap';
-import _ from 'lodash';
-import { Line } from 'react-chartjs-2';
-
+import React, { PureComponent } from 'react'
+import { Table } from 'reactstrap'
+import _ from 'lodash'
+import { Line } from 'react-chartjs-2'
 
 class ParentComponent extends PureComponent {
-    constructor() {
-        super();
+  constructor() {
+    super()
 
-        this.state = {
-            expandedRows : []
-        };
+    this.state = {
+      expandedRows: []
     }
+  }
 
-    handleRowClick(rowId) {
-        const currentExpandedRows = this.state.expandedRows;
-        const isRowCurrentlyExpanded = currentExpandedRows.includes(rowId);
+  handleRowClick(rowId) {
+    const currentExpandedRows = this.state.expandedRows
+    const isRowCurrentlyExpanded = currentExpandedRows.includes(rowId)
 
-        const newExpandedRows = isRowCurrentlyExpanded ?
-			currentExpandedRows.filter(id => id !== rowId) :
-			currentExpandedRows.concat(rowId);
+    const newExpandedRows = isRowCurrentlyExpanded
+      ? currentExpandedRows.filter(id => id !== rowId)
+      : currentExpandedRows.concat(rowId)
 
-        this.setState({expandedRows : newExpandedRows});
-    }
+    this.setState({ expandedRows: newExpandedRows })
+  }
 
-    renderItem(item,index) {
+  renderItem(item, index) {
+    var arrayCases = _.flatMap(item)
 
-        var arrayCases = _.flatMap(item)
+    var activeCases = parseInt(arrayCases[arrayCases.length - 1])
 
-        var activeCases = parseInt(arrayCases[arrayCases.length-1])
+    var yesterdaysCases = arrayCases[arrayCases.length - 2]
+    var newCases = activeCases - yesterdaysCases
 
-        var yesterdaysCases = arrayCases[arrayCases.length-2]
-        var newCases = activeCases - yesterdaysCases
+    var percentDiff = parseInt((newCases / yesterdaysCases) * 100) + '%'
 
-        var percentDiff = parseInt(newCases/yesterdaysCases * 100) + '%'
+    const clickCallback = () => this.handleRowClick(index)
+    const itemRows = [
+      <tr onClick={clickCallback} key={'row-data-' + index}>
+        <td>{item.State}</td>
+        <th> {item['County Name']}</th>
+        <th> {activeCases.toLocaleString()}</th>
+        <th> {newCases.toLocaleString()}</th>
+        <th> {percentDiff}</th>
+      </tr>
+    ]
 
+    var keysFlat = _.keys(item).slice(-100)
 
+    keysFlat = _.map(keysFlat, item =>
+      item
+        .replace('_', '/')
+        .replace('_', '/')
+        .slice(0, -3)
+    )
+    // console.log(keysFlat);
+    // console.log
+    let itemsFlat = _.values(item).slice(-100)
+    var dailyDiff = itemsFlat.map((rank, i, arr) => {
+      return arr[i] - arr[i - 1]
+    })
 
-        const clickCallback = () => this.handleRowClick(index);
-        const itemRows = [
-        			<tr onClick={clickCallback} key={"row-data-" + index}>
-        			    <td>{item.State}</td>
-                  <th> {item['County Name']}</th>
-                  <th> {activeCases.toLocaleString()}</th>
-                  <th> {newCases.toLocaleString()}</th>
-                  <th> {percentDiff}</th>
-        			</tr>
-        ];
-
-        let keysFlat = _.keys(item ).slice(-18);
-
-        let itemsFlat = _.values(item).slice(-18)
-        var dailyDiff = itemsFlat.map((rank, i, arr) => {
-                              return (arr[i] - arr[i-1])
-                      });
-
-        let datab = {
-          labels: keysFlat,
-          datasets: [{
-            label: 'Total Cases in ' + item['County Name'] + '/' + item['State'],
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: itemsFlat,
-            fill: false,
-            type: 'line'
-          },
-          {
-            label: 'New Cases in ' + item['County Name'] + '/' + item['State'],
-            backgroundColor: 'rgb(2, 99, 132)',
-            borderColor: 'rgb(2, 99, 132)',
-            data: dailyDiff,
-            fill: true,
-            borderDash: [],
-            type: 'line'
-          },
-
-
-        ]
-    }
-
-
-
-        if(this.state.expandedRows.includes(index)) {
-            itemRows.push(
-                <tr key={"row-expanded-" + index}>
-
-
-                   <td colSpan="5">
-
-                     < Line data={datab} />
-                   </td>
-                </tr>
-            );
+    let datab = {
+      labels: keysFlat,
+      datasets: [
+        {
+          label: 'Total Cases in ' + item['County Name'] + '/' + item['State'],
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: itemsFlat,
+          fill: false,
+          type: 'line'
+        },
+        {
+          label: 'New Cases in ' + item['County Name'] + '/' + item['State'],
+          backgroundColor: 'rgb(2, 99, 132)',
+          borderColor: 'rgb(2, 99, 132)',
+          data: dailyDiff,
+          fill: true,
+          borderDash: [],
+          type: 'line'
         }
-
-        return itemRows;
+      ]
     }
 
-    render() {
-        const { launches } = this.props
-        let allItemRows = [];
-
-
-        launches.map((item, index) => {
-
-          if (_.has(item,"")) {
-            delete item[""]
-          }
-          const perItemRows = this.renderItem(item,index);
-          allItemRows = allItemRows.concat(perItemRows);
-        });
-
-        return (
-          <Table className="main_table" hover responsive={true} >
-          <thead>
-            <tr>
-                  <th>State</th>
-                  <th>County</th>
-                  <th>Active Cases</th>
-                  <th>New Cases yesterday</th>
-                  <th>Day on Day Change</th>
-            </tr>
-            </thead>
-            <tbody>
-
-
-           {allItemRows}
-           </tbody>
-
-           </Table>
-        );
+    if (this.state.expandedRows.includes(index)) {
+      itemRows.push(
+        <tr key={'row-expanded-' + index}>
+          <td colSpan="5">
+            <Line data={datab} />
+          </td>
+        </tr>
+      )
     }
+
+    return itemRows
+  }
+
+  render() {
+    const { launches } = this.props
+    let allItemRows = []
+
+    launches.map((item, index) => {
+      if (_.has(item, '')) {
+        delete item['']
+      }
+      const perItemRows = this.renderItem(item, index)
+      allItemRows = allItemRows.concat(perItemRows)
+    })
+
+    return (
+      <Table className="main_table" hover responsive={true}>
+        <thead>
+          <tr>
+            <th>State</th>
+            <th>County</th>
+            <th>Confirmed Cases*</th>
+            <th>New Cases yesterday</th>
+            <th>Day on Day Change</th>
+          </tr>
+        </thead>
+        <tbody>{allItemRows}</tbody>
+      </Table>
+    )
+  }
 }
 
 export default ParentComponent
--- src/redux/actions/page.js
+++ src/redux/actions/page.js
@@ -1,5 +1,5 @@
-import Papa from 'papaparse';
-import _ from 'lodash';
+import Papa from 'papaparse'
+import _ from 'lodash'
 
 import {
   GET_LAUNCHES_REQUEST,
@@ -10,12 +10,9 @@ import {
 } from '../constants/page'
 
 function setLaunches(data) {
-
   const launches = data.data.map(launches => {
     return launches
   })
-
-
 
   return {
     type: SET_LAUNCHES,
@@ -23,18 +20,16 @@ function setLaunches(data) {
   }
 }
 
-  export function getLaunches() {
+export function getLaunches() {
   return dispatch => {
     dispatch({
       type: GET_LAUNCHES_REQUEST
     })
 
-
-    return fetch(`https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv`)
-
-
+    return fetch(
+      `https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv`
+    )
       .then(response => {
-
         if (response.ok) {
           return response.text()
         }
@@ -42,35 +37,32 @@ function setLaunches(data) {
         throw new Error(`${response.status}: ${response.statusText}`)
       })
       .then(data => {
+        var datas = data.replace(new RegExp('/', 'g'), '_')
 
-        var datas = data.replace(new RegExp('/', 'g'), '_');
-
-        var parsedJson = Papa.parse(datas , { header : true, transformHeader:true,
-        });
-
+        var parsedJson = Papa.parse(datas, {
+          header: true,
+          transformHeader: true
+        })
 
         var allUSA = {
-            'County Name': 'USA',
-            'State': 'All'
-          }
-        var dates = _.flatMap(_.keys(parsedJson.data[0])).slice(-18)
+          'County Name': 'USA',
+          State: 'All'
+        }
+        var dates = _.flatMap(_.keys(parsedJson.data[0])).slice(-130)
         console.log(dates)
-         _.forEach(dates, date => {
-           allUSA[date] = _.sum(parsedJson.data.map((item) => {
-                                if (_.isNaN(parseInt(item[date]))) {
-                                  return 0
-                                } else {
-                                 return parseInt(item[date])
-                               }
-                         }));
-          }
-
-
+        _.forEach(dates, date => {
+          allUSA[date] = _.sum(
+            parsedJson.data.map(item => {
+              if (_.isNaN(parseInt(item[date]))) {
+                return 0
+              } else {
+                return parseInt(item[date])
+              }
+            })
           )
+        })
 
-
-        parsedJson.data.push(allUSA);
-
+        parsedJson.data.push(allUSA)
 
         dispatch({
           type: GET_LAUNCHES_SUCCESS
@@ -92,39 +84,39 @@ const baseQueryState = {
   isLaunchSuccess: null,
   isUSOnly: null,
   isWithReddit: null,
-  searchQuery: '',
+  searchQuery: ''
 }
 
 export function filterLaunches(state = baseQueryState) {
   return (dispatch, getState) => {
+    let displayedLaunches = getState().page.launches
 
-    let displayedLaunches = getState().page.launches;
+    displayedLaunches = displayedLaunches.filter(launch =>
+      _.has(launch, 'County Name')
+    )
 
-    displayedLaunches = displayedLaunches.filter(launch =>_.has(launch,'County Name') );
-
-    var lastEntry = displayedLaunches[displayedLaunches.length-1]
+    var lastEntry = displayedLaunches[displayedLaunches.length - 1]
     var lastKey = _.findLastKey(lastEntry)
 
-
-    displayedLaunches = _.reverse(_.sortBy( displayedLaunches, [object => parseInt(object[lastKey])]));
+    displayedLaunches = _.reverse(
+      _.sortBy(displayedLaunches, [object => parseInt(object[lastKey])])
+    )
     // displayedLaunches = _.sortBy(displayedLaunchs, [function(o) { return o.user; }]
     if (state.isUSOnly) {
-
       displayedLaunches = displayedLaunches.filter(launch => {
-        return launch.Province_State.toLowerCase().includes("us")
+        return launch.Province_State.toLowerCase().includes('us')
       })
     }
 
-
     if (state.searchQuery) {
       displayedLaunches = displayedLaunches.filter(launch => {
-        if (launch['County Name'] ) {
-
-        return launch['County Name'].toLowerCase().includes(state.searchQuery.toLowerCase())
-      } else {
-        return false
-
-      }
+        if (launch['County Name']) {
+          return launch['County Name']
+            .toLowerCase()
+            .includes(state.searchQuery.toLowerCase())
+        } else {
+          return false
+        }
       })
     }
 
